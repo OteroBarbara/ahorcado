cc849806501ebc531f9996934e4b2ea2456ba94d
OteroBarbara <oterobarbara251@gmail.com>
2021-06-27 06:50:03 -0300

    Primer commit

---
+++ frontend/public/js/script.js
@@ -0,0 +1,73 @@
+let historial = "";
+var idgame;
+
+function cargarPalabra(){
+    let palabra = document.getElementById('palabra').value.toUpperCase(); 
+
+    if (palabra != "") {
+        console.log(palabra);
+        fetch("/ahorcado/newgame", {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({pal: palabra}),
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.idpartida);
+            document.getElementById('compartir-link').innerHTML = "Reta a un jugador a adivinar la palabra compartiéndole el siguiente link:<br>" + location.href + "2?idPartida=" + data.idpartida;
+            idgame = data.idpartida;
+            document.getElementById('consultaEstado').innerHTML = '<p>Si deseas que se se avise cuando el otro jugador termina el juego, confirma apretando el siguiente botón</p><button onclick="pedirEstado()">Confirmar</button>';
+        });
+    }else{
+        alert("El campo palabra está vacío");
+    }
+}
+
+function cargarPlayer2() {
+    fetch(`/ahorcado2/newgame${location.search}`)
+    .then(res => res.json())
+    .then(data => {
+        iniciarJuego(data.palabraOculta, data.intentos);
+    });
+}
+
+function iniciarJuego(palabra, intentos) {
+    document.getElementById('palabraOculta').innerHTML = palabra;
+    document.getElementById('intentos').innerHTML = intentos;
+}
+
+function probarLetra() {
+    let letra = document.getElementById("letra").value.toUpperCase();
+    if ((letra.length == 1)&&(letra >= 'A')&&(letra <= 'Z')){
+        console.log(letra);
+        historial += letra + ' ';
+        document.getElementById('historial').innerHTML = historial;
+        fetch(`/ahorcado2/newgame${location.search}`, {
+            method: 'POST',
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({letter: letra}),
+        })
+        .then(res =>  res.json())
+        .then(data => {
+            if (data != undefined){
+                document.getElementById('palabraOculta').innerHTML = data.palabraOculta;
+                document.getElementById('intentos').innerHTML = data.intentos;
+                if (data.adivino === true){
+                    alert("Ganaste el juego");
+                }else if (data.intentos === 0){
+                    alert("Perdiste el juego");
+                }
+            }else{
+                throw Error();
+            }
+        })
+        .catch(err => alert("Se agotaron los intentos"));
+    }else{
+        alert('Debe ingresar una letra');
+    }
+    
+}
---
+++ index.js
@@ -0,0 +1,115 @@
+const express = require('express');
+const app = express();
+const fs = require("fs");
+const PORT = 3001;
+const path = require('path');
+const shortID = require('shortid');
+
+console.log("Iniciado");
+app.use(express.static(path.resolve(__dirname,'frontend/public')));
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}))
+
+//routes
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend/index.html'))
+})
+
+app.get('/ahorcado', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend/nuevapartida.html'));
+})
+
+app.get('/ahorcado2', (req, res) => {
+    if (req.query.idPartida != undefined) {
+        res.sendFile(path.resolve(__dirname, 'frontend/player2.html'))
+    }else{
+        res.send('error 404 not found');
+    }
+})
+
+app.get('/ahorcado2/newgame', (req, res) => {
+    let id = req.query.idPartida;
+    if (id != undefined){
+        let partida = JSON.parse(fs.readFileSync("game.json")).find(game => game.idPartida == id);
+        res.json(partida);
+    }else{
+        res.send('error 404 not found');
+    }
+})
+
+//juego
+app.post('/ahorcado/newgame', (req, res) => {
+    // instrucciones cuando envía una palabra
+    let word = req.body.pal;
+    let secretword = "";
+    for (let i = 0 ; i < word.length ; i++){
+        secretword += "_";
+    };
+    let codigo = shortID.generate();
+    let infoPartida = {
+        idPartida: codigo,
+        intentos: 5,
+        palabra: word,
+        palabraOculta: secretword,
+        adivino: false
+    }
+    let data = JSON.parse(fs.readFileSync('game.json'));
+    data.push(infoPartida);
+    fs.writeFile('game.json', JSON.stringify(data), (err) => {console.log(err)});
+    return res.json({palabra: word.pal , idpartida: codigo});
+})
+
+app.post('/ahorcado2/newgame', (req, res) => {
+    let id = req.query.idPartida;
+    let letra = req.body.letter;
+    if (id != undefined){
+        let partidas = JSON.parse(fs.readFileSync("game.json"));
+        let partidaEnCurso = partidas.find(game => game.idPartida == id);
+        if(partidaEnCurso.intentos > 0){
+                let word = partidaEnCurso.palabra;
+            if (word.includes(letra)){
+                let palabra = word.split('');
+                let palabraOculta = partidaEnCurso.palabraOculta.split('');
+                for (let i=0; i<palabra.length; i++){
+                    if(palabra[i]==letra){
+                        palabraOculta[i] = letra;
+                    }
+                }
+                let secretword = palabraOculta.join('');
+                partidaEnCurso.palabraOculta = secretword;
+                if (word == secretword){
+                    partidaEnCurso.adivino = true;
+                }
+            }else{
+                partidaEnCurso.intentos --;
+            }
+            fs.writeFile('game.json', JSON.stringify(partidas), (err) => {console.log(err)});
+            res.json(partidaEnCurso);
+        }else{
+            res.send(undefined);
+        }   
+    }else{
+        res.end();
+    }
+})
+
+//consulta de estado
+app.get('/ahorcado/consultaEstado', (req, res) => {
+    let partidas = JSON.parse(fs.readFileSync("game.json"));
+    let partidaEnCurso = partidas.find(game => game.idPartida == req.query.idPartida);
+    if (partidaEnCurso.adivino == true){
+        res.json({estado: true});
+    }else if (partidaEnCurso.intentos == 0){
+        res.json({estado: false});
+    }else{
+        res.send(undefined);
+    }
+})
+
+
+//escuchando al puerto
+app.listen(PORT, () => { 
+    console.log(`Server running on port ${PORT}`) 
+});
